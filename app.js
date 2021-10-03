18a08a7a219d121bb21c6859883a73ea862f5baa
jaishilbhavsar <jaishilbhavsar@yahoo.in>
2021-10-02 22:26:36 -0400

    "Image changes"

--- app.js
+++ app.js
@@ -8,51 +8,52 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join( __dirname,'/views/home.html'));
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 app.get('/assets/logo', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/logo.jpeg'));
+  res.sendFile(path.join(__dirname, '/assets/logo.jpeg'));
 });
 app.get('/assets/hero', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/hero3.jpeg'));
+  res.sendFile(path.join(__dirname, '/assets/hero3.jpeg'));
 });
 app.get('/assets/plan', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/hero1.jpg'));
+  res.sendFile(path.join(__dirname, '/assets/hero1.jpg'));
 });
 app.get('/assets/cardlogo1', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/cardlogo1.png'));
+  res.sendFile(path.join(__dirname, '/assets/cardlogo1.png'));
 });
 app.get('/assets/cycle', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/cycle.png'));
+  res.sendFile(path.join(__dirname, '/assets/cycle.png'));
 });
 app.get('/assets/car', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/car.png'));
+  res.sendFile(path.join(__dirname, '/assets/car.png'));
 });
 app.get('/assets/rocket', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/rocket.png'));
+  res.sendFile(path.join(__dirname, '/assets/rocket.png'));
 });
 app.get('/assets/cardlogo2', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/cardlogo2.png'));
+  res.sendFile(path.join(__dirname, '/assets/cardlogo2.png'));
 });
 app.get('/assets/cardlogo3', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/cardlogo3.png'));
+  res.sendFile(path.join(__dirname, '/assets/cardlogo3.png'));
 });
 app.get('/assets/cardlogo4', (req, res) => {
-  res.sendFile(path.join( __dirname,'/assets/cardlogo4.png'));
+  res.sendFile(path.join(__dirname, '/assets/cardlogo4.png'));
 });
 app.get('/home', (req, res) => {
-  res.sendFile(path.join( __dirname,'/views/home.html'));
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 app.get('/plans', (req, res) => {
-  res.sendFile(path.join( __dirname,'/views/plans.html'));
+  res.sendFile(path.join(__dirname, '/views/plans.html'));
 });
 app.get('/signup', (req, res) => {
-  res.sendFile(path.join( __dirname,'/views/signup.html'));
+  res.sendFile(path.join(__dirname, '/views/signup.html'));
 });
 app.get('/login', (req, res) => {
-  res.sendFile(path.join( __dirname,'/views/login.html'));
+  res.sendFile(path.join(__dirname, '/views/login.html'));
 });
-
+app.use(express.static(path.join(__dirname, '/assets')));
+app.use(express.static(path.join(__dirname, '/views')));
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
